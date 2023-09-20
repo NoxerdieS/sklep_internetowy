a9c672bb02f3de251db8ce1cc209bf26e4b7a93e
Jakub Seroka <jseroka08@gmail.com>
2023-09-20 19:52:10 +0200

    added js login script and , fixed minor bugs

---
+++ php/login.php
@@ -0,0 +1,18 @@
+<?php
+// $login = $_POST['login'];
+// $password = $_POST['password'];
+
+header('Content-Type: application/json');
+
+echo json_encode([
+    'email' => 'loasdsa'
+]);
+
+http_response_code(400);
+
+header('Content-Type: application/json');
+
+echo json_encode([
+    'email' => 'loasdsa'
+]);
+?>
