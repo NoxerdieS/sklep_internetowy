5ac0fd24fd9f744099300fc30accc3557b0fb2bb
Jakub Seroka <jseroka08@gmail.com>
2023-09-30 09:20:20 +0200

    little fix to latest commit

--- js/admin_panel.js
+++ js/admin_panel.js
@@ -5,10 +5,4 @@ fetch('http://localhost/sklep_internetowy/php/checkIfAdmin.php')
 }).then(function (body){
     result = body;
     document.body.innerHTML = result;
-    if(result == '0'){
-        window.location.replace('../html/login_page.html');
-    }else{
-        console.log(result);
-        // dodaj wyświetlanie result na stronie (login używtkownika)
-    }
 })
