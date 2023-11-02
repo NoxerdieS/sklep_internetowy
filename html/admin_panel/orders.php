bbae6e2c46b0f11b8a0bd491d1b980ebb4c01268
Mateusz <mbonat42@gmail.com>
2023-11-02 17:03:49 +0100

    added payment, order and shipping forms

--- html/admin_panel/orders.php
+++ html/admin_panel/orders.php
@@ -1,7 +1,18 @@
 <?php
 
 ob_start();
-
+require_once('../../php/dblogin.php');
+$pdo = new PDO('mysql:host='.$host.';dbname='.$db.';port=3306', $user, $pass);
+?>
+<div class="admin__add">
+        <button class="admin__add--addBtn">Dodaj</button>
+        <div class="nav__user--search admin__add--search">
+          <input type="text" placeholder="Wyszukaj..." id="searchBar"/><i id="searchBtn" class="fa-solid fa-magnifying-glass"></i>
+        </div>
+</div>
+<div class="admin__products">
+<?php
+// $query = $pdo -> query('select category_name from category;');
 // while ($row = $query->fetch()){
 //     $html = '<div class="admin__product">
 //     <img src="'.$row['path'].'" alt="" class="admin__product--img">
@@ -11,7 +22,21 @@ ob_start();
 //     </div>';
 //     echo $html;
 // }
-
+?>
+    <div class="admin__popup">
+      <div class="admin__contentContainer">
+        <button class="admin__contentContainer--closeBtn"><i class="fa-solid fa-x"></i></button>
+        <form id="create-product-form" method="post">
+            <!-- <div class="admin__formContainer">
+                <label for="name">Nazwa kategorii:</label>
+                <input type="text" name="name" id="name" class="admin__contentContainer--input" placeholder="Nazwa kategorii">
+            </div> -->
+            <button type="submit" class="admin__contentContainer--addProduct">Dodaj</button>
+        </form>
+      </div>
+    </div>
+</div>
+<?php
 $body=ob_get_contents(); 
 ob_end_clean();
 
--- html/admin_panel/payment.php
+++ html/admin_panel/payment.php
@@ -1,8 +1,42 @@
 <?php
 ob_start();
-
-
-
+require_once('../../php/dblogin.php');
+$pdo = new PDO('mysql:host='.$host.';dbname='.$db.';port=3306', $user, $pass);
+?>
+<div class="admin__add">
+        <button class="admin__add--addBtn">Dodaj</button>
+        <div class="nav__user--search admin__add--search">
+          <input type="text" placeholder="Wyszukaj..." id="searchBar"/><i id="searchBtn" class="fa-solid fa-magnifying-glass"></i>
+        </div>
+</div>
+<div class="admin__products">
+<?php
+// $query = $pdo -> query('select category_name from category;');
+// while ($row = $query->fetch()){
+//     $html = '<div class="admin__product admin__payment">
+//     <img src="'.$row['path'].'" alt="" class="admin__product--img">
+//     <p class="admin__product--name">'.$row['product_name'].'</p>
+//     <a class="admin__add--addBtn">Edytuj</a>
+//     <button class="admin__add--addBtn">Anuluj</button>
+//     <button class="admin__add--addBtn">Wyłącz</button>
+//     </div>';
+//     echo $html;
+// }
+?>
+    <div class="admin__popup">
+      <div class="admin__contentContainer">
+        <button class="admin__contentContainer--closeBtn"><i class="fa-solid fa-x"></i></button>
+        <form id="create-product-form" method="post">
+            <div class="admin__formContainer">
+                <label for="name">Nazwa sposobu płatności:</label>
+                <input type="text" name="name" id="name" class="admin__contentContainer--input" placeholder="Nazwa sposobu płatności">
+            </div>
+            <button type="submit" class="admin__contentContainer--addProduct">Dodaj</button>
+        </form>
+      </div>
+    </div>
+</div>
+<?php
 $body=ob_get_contents(); 
 ob_end_clean();
 
