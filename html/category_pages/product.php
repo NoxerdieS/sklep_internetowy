e18011459ab614a4252b3d22db8c4dd3943ab14a
Mateusz Bonat <mbonat42@gmail.com>
2023-11-28 15:43:05 +0100

    small changes

--- html/category_pages/product.php
+++ html/category_pages/product.php
@@ -24,6 +24,10 @@
     ></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/main.css" />
+    <!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
     <title>Sklep</title>
   </head>
   <body>
@@ -145,15 +149,19 @@
     </nav>
     <main class="product">
         <div class="product__topSection">
-            <div class="product__topSection--image"></div>
+            <div class="product__topSection--image">
+
+            </div>
             <div class="product__topSection--specs">
-                <p class="product__topSection--headline"></p>
-                <div class="product__topSection--info"></div>
+                <p class="product__topSection--headline">Nazwa produktu</p>
+                <div class="product__topSection--info">
+                  
+                </div>
             </div>
         </div>
     </main>
-    
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="../..//js/index.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="../../js/index.js"></script>
   </body>
 </html>
